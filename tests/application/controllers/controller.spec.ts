321bed3f4a8cc1257befd1fb0d7cb37e0f750a1b
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-10-08 14:22:01 -0300

    move code from tests to src

--- tests/application/controllers/controller.spec.ts
+++ tests/application/controllers/controller.spec.ts
@@ -1,30 +1,9 @@
-import { badRequest, HttpResponse, serverError } from '@/application/helpers'
-import { ValidationComposite, Validator } from '@/application/validation'
+import { HttpResponse } from '@/application/helpers'
+import { ValidationComposite } from '@/application/validation'
 import { ServerError } from '@/application/errors'
 import { mocked } from 'ts-jest/utils'
+import { Controller } from '@/application/controllers'
 
-export abstract class Controller {
-  abstract perform (httpRequest: any): Promise<HttpResponse>
-
-  buildValidators (httpRequest: any): Validator[] {
-    return []
-  }
-
-  async handle (httpRequest: any): Promise<HttpResponse> {
-    const error = this.validate(httpRequest)
-    if (error !== undefined) return badRequest(error)
-    try {
-      return await this.perform(httpRequest)
-    } catch (error) {
-      return serverError(error)
-    }
-  }
-
-  private validate (httpRequest: any): Error | undefined {
-    const validators = this.buildValidators(httpRequest)
-    return new ValidationComposite(validators).validate()
-  }
-}
 jest.mock('@/application/validation/composite')
 
 class ControllerStub extends Controller {
