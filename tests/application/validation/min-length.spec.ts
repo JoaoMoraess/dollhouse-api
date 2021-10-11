edd3a3b5e0fa86f7dd3f5ac92409fa40c7121139
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-10-11 15:37:04 -0300

    ensure MinLenght return undefined if value is okay

--- tests/application/validation/min-length.spec.ts
+++ tests/application/validation/min-length.spec.ts
@@ -23,4 +23,18 @@ describe('MinLenght', () => {
 
     expect(error).toEqual(new InvalidFieldError('any_field'))
   })
+
+  it('should return undefined value is okay', () => {
+    const sut = new MinLength(10, 10, 'any_field')
+
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+
+    const sut2 = new MinLength(15, 10, 'any_field')
+
+    const error2 = sut2.validate()
+
+    expect(error2).toBeUndefined()
+  })
 })
