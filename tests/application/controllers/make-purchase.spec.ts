6492554cca7e9bbc381dd60cb7564e7c000b7ad0
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-10-15 19:55:40 -0300

    refactor variables

--- tests/application/controllers/make-purchase.spec.ts
+++ tests/application/controllers/make-purchase.spec.ts
@@ -2,15 +2,17 @@ import { Controller } from '@/application/controllers'
 import { HttpResponse } from '@/application/helpers'
 import { Validator, ValidationBuilder, Required, RequiredString } from '@/application/validation'
 
+type Card = {
+  number: number
+  expirationMoth: number
+  expirationYear: number
+  securityCode: number
+  holderName: string
+}
+
 type HttpRequest = {
   productsIds: string[]
-  cart: {
-    number: number
-    expirationMoth: number
-    expirationYear: number
-    securityCode: number
-    holderName: string
-  }
+  card: Card
   cep: string
 }
 
@@ -27,13 +29,13 @@ class MakePurchaseController extends Controller {
     }
   }
 
-  override buildValidators ({ productsIds, cep, cart }: HttpRequest): Validator[] {
+  override buildValidators ({ productsIds, cep, card }: HttpRequest): Validator[] {
     return [
       ...ValidationBuilder.of({ fieldValue: productsIds, fieldName: 'productsIds' })
         .required().build(),
       ...ValidationBuilder.of({ fieldValue: cep, fieldName: 'cep' })
         .required().build(),
-      ...ValidationBuilder.of({ fieldValue: cart, fieldName: 'cart' })
+      ...ValidationBuilder.of({ fieldValue: card, fieldName: 'card' })
         .required().build()
     ]
   }
@@ -41,35 +43,43 @@ class MakePurchaseController extends Controller {
 
 describe('MakePurchaseController', () => {
   let sut: MakePurchaseController
+  let productsIds: string[]
+  let cep: string
+  let card: Card
+
+  beforeEach(() => {
+    productsIds = [
+      'any_id',
+      'other_id'
+    ]
+    cep = '94750-000'
+    card = {
+      expirationMoth: 4,
+      expirationYear: 2021,
+      holderName: 'Joao M',
+      number: 2123123422,
+      securityCode: 876
+    }
+  })
   beforeEach(() => {
     sut = new MakePurchaseController()
   })
+
   it('should extend Controller', async () => {
     expect(sut).toBeInstanceOf(Controller)
   })
+
   it('should build validators', async () => {
     const validators = sut.buildValidators({
-      productsIds: ['any_id', 'other_id'],
-      cep: '94750-000',
-      cart: {
-        expirationMoth: 4,
-        expirationYear: 2021,
-        holderName: 'Joao M',
-        number: 2123123422,
-        securityCode: 876
-      }
+      productsIds,
+      cep,
+      card
     })
 
     expect(validators).toEqual([
-      new Required(['any_id', 'other_id'], 'productsIds'),
-      new RequiredString('94750-000', 'cep'),
-      new Required({
-        expirationMoth: 4,
-        expirationYear: 2021,
-        holderName: 'Joao M',
-        number: 2123123422,
-        securityCode: 876
-      }, 'cart')
+      new Required(productsIds, 'productsIds'),
+      new RequiredString(cep, 'cep'),
+      new Required(card, 'card')
     ])
   })
 })
