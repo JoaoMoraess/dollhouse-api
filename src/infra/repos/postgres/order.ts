0a708ac7322c8e41c181f1ee4f6a098b7a38ab41
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-11-07 17:37:53 -0300

    ensure PgOrderRepossitory save the order and save the OrderProducts

--- src/infra/repos/postgres/order.ts
+++ src/infra/repos/postgres/order.ts
@@ -1,8 +1,30 @@
 import { Repository } from './repository'
 import { SaveOrder } from '@/domain/contracts/repos'
+import { Order } from './entities/Order'
+import { OrderProduct } from './entities/OrderProduct'
+import { v4 } from 'uuid'
 
 export class PgOrderRepository extends Repository implements SaveOrder {
-  async save ({ cep, deliveryCost, pagSeguroId, products, subTotal, total }: SaveOrder.Input): Promise<void> {
+  private readonly orderRepository = this.getRepository<Order>('Order')
+  private readonly orderProductRepository = this.getRepository<OrderProduct>('OrderProduct')
 
+  async save ({ products, ...orderProps }: SaveOrder.Input): Promise<void> {
+    const order = this.orderRepository.create({
+      ...orderProps,
+      id: v4(),
+      confirmed: false,
+      sent: false
+    })
+
+    const orderProducts = products.map(({ productId, quantity }) => {
+      return this.orderProductRepository.create({
+        id: v4(),
+        order: order.id,
+        product: productId,
+        quantity
+      })
+    })
+    await this.orderRepository.persistAndFlush(order)
+    await this.orderProductRepository.persistAndFlush(orderProducts)
   }
 }
---
+++ tests/infra/repos/postgres/order.spec.ts
@@ -0,0 +1,97 @@
+import { IBackup } from 'pg-mem'
+import { PgOrderRepository } from '@/infra/repos/postgres'
+import { makeFakeDb } from '../mocks/connection'
+import { PgConnection } from '@/infra/repos/postgres/helpers/connection'
+import { Repository } from '@/infra/repos/postgres/repository'
+import { Order } from '@/infra/repos/postgres/entities/Order'
+import { Product } from '@/infra/repos/postgres/entities/Product'
+import { OrderProduct } from '@/infra/repos/postgres/entities/OrderProduct'
+import { EntityRepository } from '@mikro-orm/core'
+
+describe('PgOrderRepository', () => {
+  let sut: PgOrderRepository
+  let connection: PgConnection
+  let backup: IBackup
+  let pgProductRepo: EntityRepository<Product>
+  let pgOrderRepo: EntityRepository<Order>
+  let pgOrderProductRepo: EntityRepository<OrderProduct>
+
+  beforeAll(async () => {
+    const { db, orm } = await makeFakeDb([Product, Order, OrderProduct])
+    connection = PgConnection.getInstance(orm)
+    await connection.connect()
+    backup = db.backup()
+    pgProductRepo = connection.getRepository<Product>('Product')
+    pgOrderRepo = connection.getRepository<Order>('Order')
+    pgOrderProductRepo = connection.getRepository<OrderProduct>('OrderProduct')
+  })
+
+  afterAll(async () => {
+    await connection.disconnect()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PgOrderRepository()
+  })
+
+  it('should extend PgRepository', async () => {
+    expect(sut).toBeInstanceOf(Repository)
+  })
+
+  describe('save', () => {
+    it('should save the order', async () => {
+      const ids = ['1234', '4321']
+
+      const product1 = pgProductRepo.create({
+        id: ids[0],
+        imageUrl: 'any_image_url',
+        name: 'any_name',
+        price: 2390,
+        stock: 12
+      })
+      const product2 = pgProductRepo.create({
+        id: ids[1],
+        imageUrl: 'other_image_url',
+        name: 'other_name',
+        price: 2930,
+        stock: 16
+      })
+      await pgProductRepo.persistAndFlush([product1, product2])
+
+      await sut.save({
+        cep: 'any_cep',
+        deliveryCost: 123,
+        pagSeguroId: 'any_pagseguro_id',
+        products: [{
+          productId: ids[0],
+          quantity: 2
+        },
+        {
+          productId: ids[1],
+          quantity: 4
+        }],
+        subTotal: 1234,
+        total: 1740
+      })
+
+      const orderProducts = await pgOrderProductRepo.findAll()
+      const orders = await pgOrderRepo.findAll()
+
+      expect(orderProducts[0].order).toEqual({ id: orders[0].id })
+      expect(orderProducts[0].product.id).toBe(product1.id)
+      expect(orderProducts[0].quantity).toBe(2)
+      expect(orderProducts[1].order).toEqual({ id: orders[0].id })
+      expect(orderProducts[1].product.id).toBe(product2.id)
+      expect(orderProducts[1].quantity).toBe(4)
+
+      expect(orders[0].cep).toBe('any_cep')
+      expect(orders[0].confirmed).toBeFalsy()
+      expect(orders[0].deliveryCost).toBe(123)
+      expect(orders[0].pagSeguroId).toBe('any_pagseguro_id')
+      expect(orders[0].sent).toBeFalsy()
+      expect(orders[0].subTotal).toBe(1234)
+      expect(orders[0].total).toBe(1740)
+    })
+  })
+})
