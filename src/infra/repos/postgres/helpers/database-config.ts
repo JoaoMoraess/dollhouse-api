5de011a2421747d6aa1d332d5dd0ea53ae82c2f4
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-11-09 13:53:46 -0300

    add MikroOrm migration

--- src/infra/repos/postgres/helpers/database-config.ts
+++ src/infra/repos/postgres/helpers/database-config.ts
@@ -6,6 +6,17 @@ const options: Options = {
   type: 'postgresql',
   entities: ['dist/infra/repos/postgres/entities/**/*.js'],
   entitiesTs: ['src/infra/repos/postgres/entities/**/*.ts'],
-  metadataProvider: TsMorphMetadataProvider
+  metadataProvider: TsMorphMetadataProvider,
+  migrations: {
+    tableName: 'mikro_orm_migrations', // name of database table with log of executed transactions
+    path: 'src/infra/repos/postgres/migrations', // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.ts$/, // regex pattern for the migration files
+    transactional: true, // wrap each migration in a transaction
+    disableForeignKeys: true, // wrap statements with `set foreign_key_checks = 0` or equivalent
+    allOrNothing: true, // wrap all migrations in master transaction
+    dropTables: true, // allow to disable table dropping
+    safe: false, // allow to disable table and column dropping
+    emit: 'ts' // migration generation mode
+  }
 }
 export default options
---
+++ src/infra/repos/postgres/migrations/Migration20211109165036.ts
@@ -0,0 +1,17 @@
+import { Migration } from '@mikro-orm/migrations'
+
+export class Migration20211109165036 extends Migration {
+  async up (): Promise<void> {
+    this.addSql('create table "Product" ("id" text not null, "name" text not null, "stock" int4 not null, "price" int4 not null, "imageUrl" text not null);')
+    this.addSql('alter table "Product" add constraint "Product_pkey" primary key ("id");')
+
+    this.addSql('create table "Order" ("id" text not null, "total" int4 not null, "cep" text not null, "pagSeguroId" text not null, "confirmed" bool not null, "sent" bool not null, "deliveryCost" int4 not null, "subTotal" int4 not null);')
+    this.addSql('alter table "Order" add constraint "Order_pkey" primary key ("id");')
+
+    this.addSql('create table "OrderProduct" ("id" text not null, "product" text not null, "quantity" int4 not null, "order" text not null);')
+    this.addSql('alter table "OrderProduct" add constraint "OrderProduct_pkey" primary key ("id");')
+
+    this.addSql('alter table "OrderProduct" add constraint "OrderProduct_product_foreign" foreign key ("product") references "Product" ("id") on update cascade;')
+    this.addSql('alter table "OrderProduct" add constraint "OrderProduct_order_foreign" foreign key ("order") references "Order" ("id") on update cascade on delete cascade;')
+  }
+}
