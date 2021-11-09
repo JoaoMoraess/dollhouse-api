cb79f42f24df39fb713a5177349ada3702a49dcf
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-11-09 09:03:18 -0300

    ensure DbTransactionController calls openTransaction

---
+++ tests/application/decorators/db-transaction.controller.spec.ts
@@ -0,0 +1,27 @@
+import { DbTransactionController } from '@/application/decorators'
+import { Controller } from '@/application/controllers'
+import { DbTransaction } from '@/application/contracts'
+import { mock, MockProxy } from 'jest-mock-extended'
+
+describe('DbTransactionController', () => {
+  let sut: DbTransactionController
+  let decoratee: MockProxy<Controller>
+  let db: MockProxy<DbTransaction>
+
+  beforeAll(() => {
+    decoratee = mock<Controller>()
+    decoratee.perform.mockResolvedValue({ statusCode: 200, data: null })
+    db = mock<DbTransaction>()
+  })
+
+  beforeEach(() => {
+    sut = new DbTransactionController(decoratee, db)
+  })
+
+  it('should call db.openTransaction()', async () => {
+    await sut.perform({ any: 'any' })
+
+    expect(db.openTransaction).toHaveBeenCalled()
+    expect(db.openTransaction).toHaveBeenCalledTimes(1)
+  })
+})
