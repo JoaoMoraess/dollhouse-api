3e8c82ab83a9ffe34441bdf6fea1f38250945be2
JoaoMoraess <joaomorais.cirne@gmail.com>
2021-11-09 11:18:00 -0300

    ensure PgConnection.openTransaction calls MikroOrm.begin

--- tests/infra/repos/postgres/helpers/connection.spec.ts
+++ tests/infra/repos/postgres/helpers/connection.spec.ts
@@ -1,18 +1,20 @@
-import { MikroORM } from '@mikro-orm/core'
-import { IBackup } from 'pg-mem'
-import { makeFakeDb } from '../../mocks/connection'
+import { Connection, IDatabaseDriver, MikroORM } from '@mikro-orm/core'
 import { Product } from '@/infra/repos/postgres/entities/Product'
 import { PgConnection } from '@/infra/repos/postgres/helpers/connection'
+import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('Connection', () => {
   let sut: PgConnection
-  let backup: IBackup
-  let orm: MikroORM
+  let orm: MockProxy<MikroORM>
+  let connectionSpy: MockProxy<IDatabaseDriver<Connection>>
 
   beforeAll(async () => {
-    const fakeDb = await makeFakeDb([Product])
-    orm = fakeDb.orm
-    backup = fakeDb.db.backup()
+    connectionSpy = mock<IDatabaseDriver<Connection>>()
+    orm = mock<MikroORM>()
+    orm.em.getRepository = jest.fn().mockReturnValue('any_repository')
+    orm.em.begin = jest.fn()
+    orm.connect.mockResolvedValue(connectionSpy)
+    connectionSpy.close.mockResolvedValueOnce()
   })
 
   afterEach(async () => {
@@ -20,17 +22,13 @@ describe('Connection', () => {
     PgConnection.orm = undefined
   })
 
-  beforeEach(() => {
-    backup.restore()
-  })
-
   it('should connect on database', async () => {
     sut = PgConnection.getInstance(orm)
 
     await sut.connect()
-    const isConnected = await orm.isConnected()
 
-    expect(isConnected).toBeTruthy()
+    expect(orm.connect).toHaveBeenCalled()
+    expect(orm.connect).toHaveBeenCalledTimes(1)
 
     await sut.disconnect()
   })
@@ -40,29 +38,34 @@ describe('Connection', () => {
 
     await sut.connect()
     await sut.disconnect()
-    const isConnected = await orm.isConnected()
-    expect(isConnected).toBeFalsy()
+    expect(connectionSpy.close).toHaveBeenCalled()
+    expect(connectionSpy.close).toHaveBeenCalledTimes(1)
   })
 
-  it('should get the repository', async () => {
+  it('should call getRepository', async () => {
     sut = PgConnection.getInstance(orm)
 
     await sut.connect()
-    const productRepository = sut.getRepository<Product>('Product')
-    const productsCount = await productRepository.count()
+    const repository = sut.getRepository<Product>('Product')
 
-    expect(productsCount).toBe(0)
+    expect(repository).toBe('any_repository')
     await sut.disconnect()
   })
 
   it('should return the instace of connection', async () => {
+    const connectionMock1 = { name: 'first_connection', close: () => null } as any
+    orm.connect.mockResolvedValueOnce(connectionMock1)
+
     sut = PgConnection.getInstance(orm)
 
+    const connectionMock2 = { name: 'second_connection', close: () => null } as any
+    orm.connect.mockResolvedValueOnce(connectionMock2)
+
     const connection = PgConnection.getInstance()
-    await connection.connect()
-    const isConnected = await orm.isConnected()
-    expect(isConnected).toBeTruthy()
-    await connection.disconnect()
+
+    await sut.connect()
+    expect(connection).toEqual({ connection: connectionMock1 })
+    await sut.disconnect()
   })
 
   it('should throw if orm is not provided', async () => {
@@ -82,5 +85,16 @@ describe('Connection', () => {
     const sut = PgConnection.getInstance(orm)
     const promise = sut.disconnect()
     await expect(promise).rejects.toThrow(new Error('Connection not found'))
+  })
+
+  it('should open transaction', async () => {
+    const sut = PgConnection.getInstance(orm)
+    await sut.connect()
+    await sut.openTransaction()
+
+    expect(orm.em.begin).toHaveBeenCalledWith()
+    expect(orm.em.begin).toHaveBeenCalledTimes(1)
+
+    await sut.disconnect()
   })
 })
