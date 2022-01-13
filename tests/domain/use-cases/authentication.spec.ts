d39128dbb02bfc30f8d13d2ec80604c0182cd696
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-13 17:48:52 -0300

    ensure Authentication returns null if usersRepo return null

--- tests/domain/use-cases/authentication.spec.ts
+++ tests/domain/use-cases/authentication.spec.ts
@@ -1,23 +1,29 @@
 import { mock, MockProxy } from 'jest-mock-extended'
 
 interface LoadUserByEmail {
-  loadByEmail: (field: string) => Promise<{id: string, name: string, password: string}>
+  loadByEmail: (field: string) => Promise<{id: string, name: string, password: string} | null>
 }
 
-type Setup = (userRepo: LoadUserByEmail) => Authentication
-export type Authentication = (input: { email: string, password: string }) => Promise<{name: string, token: string}>
+interface HashComparer {
+  compare: (plaintext: string, digest: string) => Promise<boolean>
+}
 
-export const setAuthentication: Setup = (userRepo) => async ({ email, password }) => {
-  await userRepo.loadByEmail(email)
+type Setup = (userRepo: LoadUserByEmail, hashComparer: HashComparer) => Authentication
+export type Authentication = (input: { email: string, password: string }) => Promise<{name: string, token: string} | null>
 
-  return {
-    name: 'any_name',
-    token: 'any_token'
+export const setAuthentication: Setup = (userRepo, hashComparer) => async ({ email, password }) => {
+  const user = await userRepo.loadByEmail(email)
+
+  if (user !== undefined && user !== null) {
+    console.log(user)
   }
+
+  return null
 }
 
 describe('Authentication', () => {
   let userRepo: MockProxy<LoadUserByEmail>
+  let hashComparer: MockProxy<HashComparer>
   let sut: Authentication
   let email: string
   let password: string
@@ -29,11 +35,13 @@ describe('Authentication', () => {
       name: 'any_name',
       password: 'any_password'
     })
+    hashComparer = mock()
+    hashComparer.compare.mockResolvedValue(true)
   })
   beforeEach(() => {
     email = 'any_email@gmail.com'
     password = 'any_password_123'
-    sut = setAuthentication(userRepo)
+    sut = setAuthentication(userRepo, hashComparer)
   })
 
   it('should call userRepo.loadByEmail with correct input', async () => {
@@ -41,5 +49,11 @@ describe('Authentication', () => {
 
     expect(userRepo.loadByEmail).toHaveBeenCalledWith(email)
     expect(userRepo.loadByEmail).toHaveBeenCalledTimes(1)
+  })
+  it('should return null if userRepo.loadByEmail returns undefined', async () => {
+    userRepo.loadByEmail.mockResolvedValueOnce(null)
+    const userData = await sut({ email, password })
+
+    expect(userData).toEqual(null)
   })
 })
