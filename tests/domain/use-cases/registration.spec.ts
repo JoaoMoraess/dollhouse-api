2a6cf10b5230fb276e5cacc4f84148b733fdd9d1
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 16:35:40 -0300

    ensure Registration return null if usersRepo.loadByEmail return null

--- tests/domain/use-cases/registration.spec.ts
+++ tests/domain/use-cases/registration.spec.ts
@@ -3,36 +3,45 @@ import { AuthenticationModel } from '@/domain/entities'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 export type Registration = (input: {name: string, email: string, password: string}) => Promise<AuthenticationModel | null>
-type Setup = (userRepo: LoadUserByEmail) => Registration
+type Setup = (usersRepo: LoadUserByEmail) => Registration
 
-const setRegistration: Setup = (userRepo) => async ({ email, name, password }) => {
-  await userRepo.loadByEmail({ email })
+const setRegistration: Setup = (usersRepo) => async ({ email, name, password }) => {
+  const registredUser = await usersRepo.loadByEmail({ email })
+  if (registredUser !== null && registredUser !== undefined) return null
+
   return null
 }
 
 describe('Registration', () => {
   let sut: Registration
-  let userRepo: MockProxy<LoadUserByEmail>
+  let usersRepo: MockProxy<LoadUserByEmail>
   let name: string
   let email: string
   let password: string
 
   beforeAll(() => {
-    userRepo = mock<LoadUserByEmail>()
-    userRepo.loadByEmail.mockResolvedValue(null)
+    usersRepo = mock<LoadUserByEmail>()
+    usersRepo.loadByEmail.mockResolvedValue(null)
   })
 
   beforeEach(() => {
     name = 'any_name'
     email = 'any_email@gmail.com'
     password = 'any_password'
-    sut = setRegistration(userRepo)
+    sut = setRegistration(usersRepo)
   })
 
-  it('should call userRepo.loadByEmail with correct input', async () => {
+  it('should call usersRepo.loadByEmail with correct input', async () => {
     await sut({ email, name, password })
 
-    expect(userRepo.loadByEmail).toHaveBeenCalledWith({ email })
-    expect(userRepo.loadByEmail).toHaveBeenCalledTimes(1)
+    expect(usersRepo.loadByEmail).toHaveBeenCalledWith({ email })
+    expect(usersRepo.loadByEmail).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return null if usersRepo.loadByEmail returns null', async () => {
+    usersRepo.loadByEmail.mockResolvedValueOnce(null)
+    const authenticationModel = await sut({ email, name, password })
+
+    expect(authenticationModel).toEqual(null)
   })
 })
