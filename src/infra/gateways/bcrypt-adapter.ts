9e742d3fe671ff1fec3bf0990fd2942d28e49f02
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 17:29:56 -0300

    ensure BcryptAdapter.hash calls bcrypt.hash with correct input

--- src/infra/gateways/bcrypt-adapter.ts
+++ src/infra/gateways/bcrypt-adapter.ts
@@ -1,7 +1,14 @@
-import { HashComparer } from '@/domain/contracts/gateways'
+import { HashComparer, Hasher } from '@/domain/contracts/gateways'
 import bcrypt from 'bcrypt'
 
-export class BcryptAdapter implements HashComparer {
+export class BcryptAdapter implements Hasher, HashComparer {
+  constructor (private readonly salt: number = 12) {}
+
+  async hash ({ plainText }: { plainText: string }): Promise<string> {
+    const hashedText = await bcrypt.hash(plainText, this.salt)
+    return hashedText
+  }
+
   async compare ({ plainText, digest }: { plainText: string, digest: string }): Promise<boolean> {
     const isEqual = await bcrypt.compare(plainText, digest)
     return isEqual
---
+++ tests/infra/gateways/bcrypt-adapter.spec.ts
@@ -0,0 +1,59 @@
+import bcrypt from 'bcrypt'
+import { BcryptAdapter } from '@/infra/gateways'
+
+jest.mock('bcrypt')
+
+describe('BcryptAdapter', () => {
+  let sut: BcryptAdapter
+  let fakeBcrypt: jest.Mocked<typeof bcrypt>
+  let plainText: string
+  let digest: string
+  let salt: number
+
+  beforeAll(() => {
+    salt = 12
+    fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>
+    fakeBcrypt.compare.mockImplementation(() => true)
+    fakeBcrypt.hash.mockImplementation(() => 'hashed_string')
+  })
+
+  beforeEach(() => {
+    plainText = 'any_string'
+    digest = 'any_encrypted_string'
+    sut = new BcryptAdapter(salt)
+  })
+
+  describe('hash()', () => {
+    it('should call bcrypt.hash with correct input', async () => {
+      await sut.hash({ plainText: 'any_value' })
+
+      expect(fakeBcrypt.hash).toHaveBeenCalledWith('any_value', salt)
+      expect(fakeBcrypt.hash).toHaveBeenCalledTimes(1)
+    })
+  })
+
+  describe('compare()', () => {
+    it('should call bcrypt.compare with correct input', async () => {
+      await sut.compare({ plainText, digest })
+
+      expect(fakeBcrypt.compare).toHaveBeenCalledWith(plainText, digest)
+      expect(fakeBcrypt.compare).toHaveBeenCalledTimes(1)
+    })
+    it('Should return true when compare succeeds', async () => {
+      const isValid = await sut.compare({ plainText, digest })
+      expect(isValid).toBe(true)
+    })
+
+    it('Should return false when compare fails', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(() => false)
+      const isValid = await sut.compare({ plainText, digest })
+      expect(isValid).toBe(false)
+    })
+
+    it('Should throw if compare throws', async () => {
+      fakeBcrypt.compare.mockImplementationOnce(() => { throw new Error('bcrypt_error') })
+      const promise = sut.compare({ plainText, digest })
+      await expect(promise).rejects.toThrow(new Error('bcrypt_error'))
+    })
+  })
+})
