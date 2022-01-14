e3c352956a29c3fb53afdc81c57b6b71dee2f242
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 16:43:49 -0300

    ensure Registration calls hasher.hash with correct input

--- tests/domain/use-cases/registration.spec.ts
+++ tests/domain/use-cases/registration.spec.ts
@@ -2,12 +2,18 @@ import { LoadUserByEmail } from '@/domain/contracts/repos'
 import { AuthenticationModel } from '@/domain/entities'
 import { mock, MockProxy } from 'jest-mock-extended'
 
-export type Registration = (input: {name: string, email: string, password: string}) => Promise<AuthenticationModel | null>
-type Setup = (usersRepo: LoadUserByEmail) => Registration
+export interface Hasher {
+  hash: (input: { plainText: string }) => Promise<string>
+}
 
-const setRegistration: Setup = (usersRepo) => async ({ email, name, password }) => {
+export type Registration = (input: {name: string, email: string, password: string}) => Promise<AuthenticationModel | null>
+type Setup = (usersRepo: LoadUserByEmail, hasher: Hasher) => Registration
+
+const setRegistration: Setup = (usersRepo, hasher) => async ({ email, name, password }) => {
   const registredUser = await usersRepo.loadByEmail({ email })
   if (registredUser !== null && registredUser !== undefined) return null
+
+  await hasher.hash({ plainText: password })
 
   return null
 }
@@ -15,6 +21,7 @@ const setRegistration: Setup = (usersRepo) => async ({ email, name, password }) 
 describe('Registration', () => {
   let sut: Registration
   let usersRepo: MockProxy<LoadUserByEmail>
+  let hasher: MockProxy<Hasher>
   let name: string
   let email: string
   let password: string
@@ -22,13 +29,15 @@ describe('Registration', () => {
   beforeAll(() => {
     usersRepo = mock<LoadUserByEmail>()
     usersRepo.loadByEmail.mockResolvedValue(null)
+    hasher = mock<Hasher>()
+    hasher.hash.mockResolvedValue('hashed_string')
   })
 
   beforeEach(() => {
     name = 'any_name'
     email = 'any_email@gmail.com'
     password = 'any_password'
-    sut = setRegistration(usersRepo)
+    sut = setRegistration(usersRepo, hasher)
   })
 
   it('should call usersRepo.loadByEmail with correct input', async () => {
@@ -43,5 +52,11 @@ describe('Registration', () => {
     const authenticationModel = await sut({ email, name, password })
 
     expect(authenticationModel).toEqual(null)
+  })
+  it('should call hasher with correct input', async () => {
+    await sut({ email, name, password })
+
+    expect(hasher.hash).toHaveBeenCalledWith({ plainText: password })
+    expect(hasher.hash).toBeCalledTimes(1)
   })
 })
