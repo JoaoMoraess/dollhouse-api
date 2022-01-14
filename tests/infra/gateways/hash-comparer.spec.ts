a8939aa6bc126464b97124f0e47fb0512b3474f7
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 11:44:50 -0300

    ensure BcryptAdapter.compare return true when compare succeeds

--- tests/infra/gateways/hash-comparer.spec.ts
+++ tests/infra/gateways/hash-comparer.spec.ts
@@ -18,6 +18,7 @@ describe('BcryptAdapter', () => {
 
   beforeEach(() => {
     fakeBcrypt = bcrypt as jest.Mocked<typeof bcrypt>
+    fakeBcrypt.compare.mockImplementation(() => true)
     plainText = 'any_string'
     digest = 'any_encrypted_string'
     sut = new BcryptAdapter()
@@ -28,5 +29,21 @@ describe('BcryptAdapter', () => {
 
     expect(fakeBcrypt.compare).toHaveBeenCalledWith(plainText, digest)
     expect(fakeBcrypt.compare).toHaveBeenCalledTimes(1)
+  })
+  test('Should return true when compare succeeds', async () => {
+    const isValid = await sut.compare({ plainText, digest })
+    expect(isValid).toBe(true)
+  })
+
+  test('Should return false when compare fails', async () => {
+    fakeBcrypt.compare.mockImplementationOnce(() => false)
+    const isValid = await sut.compare({ plainText, digest })
+    expect(isValid).toBe(false)
+  })
+
+  test('Should throw if compare throws', async () => {
+    fakeBcrypt.compare.mockImplementationOnce(() => { throw new Error('bcrypt_error') })
+    const promise = sut.compare({ plainText, digest })
+    await expect(promise).rejects.toThrow(new Error('bcrypt_error'))
   })
 })
