1465c7d24e25edfe3a86159105cb5157ca182d82
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 17:07:32 -0300

    ensure Registration calls tokenHandler.generate with correct input

--- tests/domain/use-cases/registration.spec.ts
+++ tests/domain/use-cases/registration.spec.ts
@@ -1,28 +1,28 @@
 import { LoadUserByEmail } from '@/domain/contracts/repos'
-import { AuthenticationModel } from '@/domain/entities'
+import { AuthenticationModel, AccessToken } from '@/domain/entities'
+import { TokenGenerator } from '@/domain/contracts/gateways'
 import { mock, MockProxy } from 'jest-mock-extended'
 
 export interface SaveUser {
-  save: (input: {name: string, email: string, password: string}) => Promise<void>
+  save: (input: {name: string, email: string, password: string}) => Promise<{id: string}>
 }
 export interface Hasher {
   hash: (input: { plainText: string }) => Promise<string>
 }
 
 export type Registration = (input: {name: string, email: string, password: string}) => Promise<AuthenticationModel | null>
-type Setup = (usersRepo: LoadUserByEmail & SaveUser, hasher: Hasher) => Registration
+type Setup = (usersRepo: LoadUserByEmail & SaveUser, hasher: Hasher, tokenHandler: TokenGenerator) => Registration
 
-const setRegistration: Setup = (usersRepo, hasher) => async ({ email, name, password }) => {
+const setRegistration: Setup = (usersRepo, hasher, tokenHandler) => async ({ email, name, password }) => {
   const registredUser = await usersRepo.loadByEmail({ email })
   if (registredUser !== null && registredUser !== undefined) return null
-
   const hashedPassword = await hasher.hash({ plainText: password })
-
-  await usersRepo.save({
+  const { id } = await usersRepo.save({
     email,
     name,
     password: hashedPassword
   })
+  tokenHandler.generate({ key: id, expirationInMs: AccessToken.expirationInMs })
 
   return null
 }
@@ -31,6 +31,7 @@ describe('Registration', () => {
   let sut: Registration
   let usersRepo: MockProxy<LoadUserByEmail & SaveUser>
   let hasher: MockProxy<Hasher>
+  let tokenHandler: MockProxy<TokenGenerator>
   let name: string
   let email: string
   let password: string
@@ -38,15 +39,18 @@ describe('Registration', () => {
   beforeAll(() => {
     usersRepo = mock<LoadUserByEmail & SaveUser>()
     usersRepo.loadByEmail.mockResolvedValue(null)
+    usersRepo.save.mockResolvedValue({ id: 'any_id' })
     hasher = mock<Hasher>()
     hasher.hash.mockResolvedValue('hashed_string')
+    tokenHandler = mock<TokenGenerator>()
+    tokenHandler.generate.mockReturnValue('any_token')
   })
 
   beforeEach(() => {
     name = 'any_name'
     email = 'any_email@gmail.com'
     password = 'any_password'
-    sut = setRegistration(usersRepo, hasher)
+    sut = setRegistration(usersRepo, hasher, tokenHandler)
   })
 
   it('should call usersRepo.loadByEmail with correct input', async () => {
@@ -73,5 +77,11 @@ describe('Registration', () => {
 
     expect(usersRepo.save).toHaveBeenCalledWith({ email, name, password: 'hashed_string' })
     expect(usersRepo.save).toBeCalledTimes(1)
+  })
+  it('should call tokenHandler.generate with correct input', async () => {
+    await sut({ email, name, password })
+
+    expect(tokenHandler.generate).toHaveBeenCalledWith({ key: 'any_id', expirationInMs: AccessToken.expirationInMs })
+    expect(tokenHandler.generate).toBeCalledTimes(1)
   })
 })
