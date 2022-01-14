243443fd79ab6c05321094cbe46950f350cb6ca3
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-14 16:21:01 -0300

    move code from tests to src

--- tests/application/controllers/registration.spec.ts
+++ tests/application/controllers/registration.spec.ts
@@ -1,39 +1,12 @@
-import { AuthenticationModel } from '@/domain/entities'
-import { Controller } from '@/application/controllers'
-import { HttpResponse, ok, unauthorized } from '@/application/helpers'
-import { Email, RequiredString, Validator, ValidationBuilder } from '@/application/validation'
+import { Controller, RegistrationController } from '@/application/controllers'
+import { ok, unauthorized } from '@/application/helpers'
+import { Email, RequiredString } from '@/application/validation'
 
 type HttpRequest = {
   name: string
   email: string
   password: string
   passwordConfirmation: string
-}
-
-type Registration = (input: { name: string, email: string, password: string }) => Promise<AuthenticationModel | null>
-
-class RegistrationController extends Controller {
-  constructor (private readonly registration: Registration) { super() }
-
-  override async perform (httpRequest: HttpRequest): Promise<HttpResponse<any>> {
-    const { email, name, password } = httpRequest
-    const authenticationModel = await this.registration({ email, name, password })
-
-    if (authenticationModel !== null && authenticationModel !== undefined) {
-      return ok(authenticationModel)
-    }
-    return unauthorized()
-  }
-
-  override buildValidators ({ name, email, password, passwordConfirmation }: HttpRequest): Validator[] {
-    // TODO fazer o compareFieldsValidator
-    return [
-      ...ValidationBuilder.of({ fieldValue: name, fieldName: 'name' }).required().build(),
-      ...ValidationBuilder.of({ fieldValue: email, fieldName: 'email' }).required().email().build(),
-      ...ValidationBuilder.of({ fieldValue: password, fieldName: 'password' }).required().build(),
-      ...ValidationBuilder.of({ fieldValue: passwordConfirmation, fieldName: 'passwordConfirmation' }).required().build()
-    ]
-  }
 }
 
 describe('RegistrationController', () => {
