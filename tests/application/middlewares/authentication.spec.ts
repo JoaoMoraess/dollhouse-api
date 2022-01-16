31aeafb716a531ada5571975fc89351530cde767
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-01-16 12:15:57 -0300

    ensure AuthenticationMIddleware not call authorize if httpRequest is not valid

--- tests/application/middlewares/authentication.spec.ts
+++ tests/application/middlewares/authentication.spec.ts
@@ -1,8 +1,16 @@
+import { RequiredString } from '@/application/validation'
+
 class AuthenticationMiddleware {
   constructor (private readonly authorize: Authorize) {}
 
   async handle ({ token }: HttpRequest): Promise<void> {
+    if (!this.validate({ token })) return
     await this.authorize({ token })
+  }
+
+  private validate ({ token }: {token: string}): boolean {
+    const error = new RequiredString(token).validate()
+    return error === undefined
   }
 }
 
@@ -25,5 +33,10 @@ describe('AuthenticationMiddleware', () => {
     await sut.handle({ token: 'any_token' })
 
     expect(authorize).toHaveBeenCalledWith({ token: 'any_token' })
+  })
+  it('should validate httpRequest correctly', async () => {
+    await sut.handle({ token: null as any })
+
+    expect(authorize).not.toHaveBeenCalled()
   })
 })
