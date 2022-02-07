7286998e4038a75f27c9ad2d7fefbdca762e2c5a
JoaoMoraess <joaomorais.cirne@gmail.com>
2022-02-07 16:44:25 -0300

    move code from tests to src

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,31 +1,8 @@
-import { DeleteFile, UploadFile } from '@/domain/contracts/gateways'
 import { config, S3 } from 'aws-sdk'
 import { mocked } from 'ts-jest/utils'
+import { AWSS3FileStorage } from '@/infra/gateways'
 
 jest.mock('aws-sdk')
-
-class AWSS3FileStorage implements UploadFile, DeleteFile {
-  constructor (accessKeyId: string, secretAccessKey: string, private readonly bucketName: string) {
-    config.update({ credentials: { accessKeyId, secretAccessKey } })
-  }
-
-  async upload ({ file, fileName }: { file: Buffer, fileName: string }): Promise<string> {
-    await new S3().putObject({
-      Bucket: this.bucketName,
-      Key: fileName,
-      Body: file,
-      ACL: 'public-read'
-    }).promise()
-    return `https://${this.bucketName}.s3.amazonaws.com/${encodeURIComponent(fileName)}`
-  }
-
-  async delete ({ fileName }: { fileName: string }): Promise<void> {
-    await new S3().deleteObject({
-      Bucket: this.bucketName,
-      Key: fileName
-    }).promise()
-  }
-}
 
 describe('AWSS3FileStorage', () => {
   let sut: AWSS3FileStorage
